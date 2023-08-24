b76abaa90feb5d58d31a7576bda19283458e068c
hemanth <hemanthtadikonda29@gmail.com>
2023-08-24 21:18:45 +0530

    parameter

--- state.tf
+++ state.tf
@@ -1,3 +1,7 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket = "tad-state"
+    key    = "aws/para"
+    region = "us-east-1"
+  }
 }
--- state.tfvars
+++
@@ -1,3 +0,0 @@
-bucket = "tad-state"
-key    = "aws/para"
-region = "us-east-1"
