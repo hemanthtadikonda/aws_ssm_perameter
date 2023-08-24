e5d1c495a86852c35a61d80412ffe30893e79146
hemanth <hemanthtadikonda29@gmail.com>
2023-08-24 15:04:00 +0530

    parameter

--- data.tf
+++ data.tf
@@ -1,7 +1,9 @@
 data "aws_ssm_parameter" "master_username" {
   name = "docdb.dev.master_username"
+  sensitive = true
 
 }
 data "aws_ssm_parameter" "master_password" {
   name = "docdb.dev.master_password"
+  sensitive = true
 }
