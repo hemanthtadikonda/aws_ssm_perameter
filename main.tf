a92e6556f760fce045e5ee31469938ab851d00d5
hemanth <hemanthtadikonda29@gmail.com>
2023-08-24 14:31:39 +0530

    parameter

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
-resource "aws_ssm_parameter" "foo" {
+resource "aws_ssm_parameter" "docdb" {
   for_each = var.docdb
   name  = each.key
   type  = each.value["type"]
   value = each.value["value"]
+}
+
+output "master_username" {
+  value = data.aws_ssm_parameter.master_username
+}
+
+output "master_password" {
+  value = data.aws_ssm_parameter.master_password
 }
