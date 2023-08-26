a6a7654fb7dac58cd9705d6ee9bcf1228639409a
hemanth <hemanthtadikonda29@gmail.com>
2023-08-26 14:47:46 +0530

    tf - module - elasticache

--- vars.tf
+++ vars.tf
@@ -3,11 +3,11 @@ variable "docdb" {
     "docdb.dev.master_username" = {  type = "String" , value = "docdbadmin"}
 
     "rds.dev.master_username"   = { type = "String" , value = "devrdsadmin"}
-    "rds.dev.database_name"     = { type = "string" , value = "dummy"}
+    "rds.dev.database_name"     = { type = "String" , value = "dummy"}
     ## Usually the passwords are not at all preferred to keep under git repo., Usually in orgs we always create passwords in manual way whomever have access. In class we keep it here for easy reference
     ## Passwords
     "docdb.dev.master_password" = { type = "String", value = "roboshop1234" }
-    "rds.dev.master_password"   = { type = "string" , value = "roboshop1234"}
+    "rds.dev.master_password"   = { type = "String" , value = "roboshop1234"}
   }
 }
 
