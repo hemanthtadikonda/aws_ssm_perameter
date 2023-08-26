3f1f991660e44b2a86c94961d1b1a18dc76ae416
hemanth <hemanthtadikonda29@gmail.com>
2023-08-26 11:59:51 +0530

    rds.dev username,passkey , dbname

--- vars.tf
+++ vars.tf
@@ -1,9 +1,13 @@
 variable "docdb" {
   default = {
     "docdb.dev.master_username" = {  type = "String" , value = "docdbadmin"}
+
+    "rds.dev.master_username"   = { type = "String" , value = "devrdsadmin"}
+    "rds.dev.database_name"     = { type = "string" , value = "dummy"}
     ## Usually the passwords are not at all preferred to keep under git repo., Usually in orgs we always create passwords in manual way whomever have access. In class we keep it here for easy reference
     ## Passwords
     "docdb.dev.master_password" = { type = "String", value = "roboshop1234" }
+    "rds.dev.master_password"   = { type = "string" , value = "roboshop1234"}
   }
 }
 
