bdbf2b28f4eedf8432ffd8407c92bf7b483d66af
hemanth <hemanthtadikonda29@gmail.com>
2023-08-30 14:47:59 +0530

    up to - apps setup completed

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "aws_ssm_parameter" "docdb" {
-  for_each = var.docdb
+resource "aws_ssm_parameter" "parameters" {
+  for_each = var.parameters
   name  = each.key
   type  = each.value["type"]
   value = each.value["value"]
--- vars.tf
+++ vars.tf
@@ -1,9 +1,11 @@
-variable "docdb" {
+variable "parameters" {
   default = {
     "docdb.dev.master_username" = {  type = "String" , value = "docdbadmin"}
-
+    "docdb.dev.endpoint"        = { type = "String" , value = }
     "rds.dev.master_username"   = { type = "String" , value = "devrdsadmin"}
     "rds.dev.database_name"     = { type = "String" , value = "dummy"}
+
+
     ## Usually the passwords are not at all preferred to keep under git repo., Usually in orgs we always create passwords in manual way whomever have access. In class we keep it here for easy reference
     ## Passwords
     "docdb.dev.master_password" = { type = "String", value = "roboshop1234" }
