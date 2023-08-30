969340792589ea48cabb3fe0e2202bca91569f20
hemanth <hemanthtadikonda29@gmail.com>
2023-08-30 14:54:26 +0530

    ansible

--- vars.tf
+++ vars.tf
@@ -2,8 +2,13 @@ variable "parameters" {
   default = {
     "docdb.dev.master_username" = {  type = "String" , value = "docdbadmin"}
     "docdb.dev.endpoint"        = { type = "String" , value = }
+
+
     "rds.dev.master_username"   = { type = "String" , value = "devrdsadmin"}
     "rds.dev.database_name"     = { type = "String" , value = "dummy"}
+
+    "user.dev.REDIS_HOST"       = { type = "String", value = "dev-redis-elasticache-cluster.ppugfs.0001.use1.cache.amazonaws.com" }
+    "cart.dev.REDIS_HOST"       = { type = "String", value = "dev-redis-elasticache-cluster.ppugfs.0001.use1.cache.amazonaws.com" }
 
 
     ## Usually the passwords are not at all preferred to keep under git repo., Usually in orgs we always create passwords in manual way whomever have access. In class we keep it here for easy reference
