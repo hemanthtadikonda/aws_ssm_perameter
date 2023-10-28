b4f593009e202747e283095eb9f548da828c6db6
hemanth <hemanthtadikonda29@gmail.com>
2023-10-28 17:56:07 +0530

    Docker files

--- vars.tf
+++ vars.tf
@@ -11,17 +11,17 @@ variable "parameters" {
     "cart.dev.REDIS_HOST"       = { type = "String", value = "dev-elasticache-cluster.2tso0w.0001.use1.cache.amazonaws.com" }
     "cart.dev.CATALOGUE_HOST"   = { type = "String", value = "catalogue-dev.tadikonda.online" }
     "cart.dev.CATALOGUE_PORT"   = { type = "String", value = "80" }
-    "shipping.dev.CART_ENDPOINT"  = { type = "String", value = "cart-dev.tadikonda.online:80" }
+   // "shipping.dev.CART_ENDPOINT"  = { type = "String", value = "cart-dev.tadikonda.online:80" }
     "shipping.dev.DB_HOST"        = { type = "String", value = "dev-mysql-rds-cluster.cluster-cec9yfajj5ic.us-east-1.rds.amazonaws.com" }
 
-    "payment.dev.CART_HOST"  = { type = "String", value = "cart-dev.tadikonda.online" }
-    "payment.dev.CART_PORT"  = { type = "String", value = "80" }
-    "payment.dev.USER_HOST"  = { type = "String", value = "user-dev.tadikonda.online" }
-    "payment.dev.USER_PORT"  = { type = "String", value = "80" }
+    "payment.dev.CART_HOST"  = { type = "String", value = "cart" }
+    "payment.dev.CART_PORT"  = { type = "String", value = "8080" }
+    "payment.dev.USER_HOST"  = { type = "String", value = "user" }
+    "payment.dev.USER_PORT"  = { type = "String", value = "8080" }
     "payment.dev.AMQP_HOST"  = { type = "String", value = "rabbitmq-dev.tadikonda.online" }
 
     "rabbitmq.dev.AMQP_USER"  = { type = "String", value = "roboshop" }
-    "rabbitmq.dev.AMQP_PASS"  = { type = "String", value = "roboshop123" }
+    "rabbitmq.dev.AMQP_PASS"  = { type = "SecureString", value = "roboshop123" }
 
 
     ## Usually the passwords are not at all preferred to keep under git repo., Usually in orgs we always create passwords in manual way whomever have access. In class we keep it here for easy reference
@@ -40,6 +40,11 @@ variable "parameters" {
 
     "docdb.dev.schema_setup"   = { type  = "SecureString", value = "mongo --host dev-docdb-cluster.cluster-cec9yfajj5ic.us-east-1.docdb.amazonaws.com:27017 --sslCAFile rds-combined-ca-bundle.pem --username docdbadmin --password roboshop1234" }
     "sql.dev.schema_setup"     = { type  = "SecureString", value = "mysql -h dev-mysql-rds-cluster.cluster-cec9yfajj5ic.us-east-1.rds.amazonaws.com -udevrdsadmin -proboshop1234" }
+
+    "shipping.dev.CART_ENDPOINT" = { type =  "String" , value = "cart:8080" }
+    "shipping.dev.DB_HOST"        = { type = "String", value = "dev-mysql-rds-cluster.cluster-cec9yfajj5ic.us-east-1.rds.amazonaws.com" }
+
+
 
 
 
