1fe07fc5080bb32b931c56a92aa6d947c5d3c5c8
hemanth <hemanthtadikonda29@gmail.com>
2023-11-03 06:45:44 +0530

    Docker file changes schema setup

--- vars.tf
+++ vars.tf
@@ -37,7 +37,7 @@ variable "parameters" {
     "catalogue.dev.DOCUMENTDB" = { type =  "String" , value = "true" }
 
 
-    "docdb.dev.schema_setup"   = { type  = "SecureString", value = "mongo --host dev-docdb-cluster.cluster-cec9yfajj5ic.us-east-1.docdb.amazonaws.com:27017 --sslCAFile rds-combined-ca-bundle.pem --username docdbadmin --password roboshop1234" }
+    "docdb.dev.schema_setup"   = { type  = "SecureString", value = "mongo --host dev-docdb-cluster.cluster-cec9yfajj5ic.us-east-1.docdb.amazonaws.com:27017 --sslCAFile /app/rds-combined-ca-bundle.pem --username docdbadmin --password roboshop1234" }
     "sql.dev.schema_setup"     = { type  = "SecureString", value = "mysql -h dev-mysql-rds-cluster.cluster-cec9yfajj5ic.us-east-1.rds.amazonaws.com -udevrdsadmin -proboshop1234" }
 
     "shipping.dev.CART_ENDPOINT" = { type = "String" , value = "cart:8080" }
